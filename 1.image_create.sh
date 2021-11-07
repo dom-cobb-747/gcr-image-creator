da11c74833bd7118c23ac8cda6997f0da9338200
Orlando Hurtado <orlando.hurtado@milwaukeetool.com>
2021-11-07 14:23:40 -0500

    added exampple function list

--- 1.image_create.sh
+++ 1.image_create.sh
@@ -3,6 +3,7 @@ APP_NAME=myapp
 /usr/local/bin/python -m pip install --upgrade pip
 #build image
 docker build -t $APP_NAME .
+docker images
 #getting $mytag
 # x="$PWD" && my_tag=${x#*home/} && echo $my_tag
 # #actually tagging 
