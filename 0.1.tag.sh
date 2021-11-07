ea0888b28876ae927379a9d52d440be8f81f92d4
Orlando Hurtado <orlando.hurtado@milwaukeetool.com>
2021-11-07 14:04:59 -0500

    added exampple function list

--- 0.1.tag.sh
+++ 0.1.tag.sh
@@ -1,3 +1,4 @@
 #playground-s-11-c37be1d8/myapp
-0.1.tag.sh playground-s-11-c37be1d8 myapp
-docker tag $2 gcr.io/$1/$2
+PROJECT_ID=$(gcloud config get-value core/project 2>/dev/null) && echo $PROJECT_ID
+#0.1.tag.sh myapp
+docker tag $1 gcr.io/$PROJECT_ID/$1
